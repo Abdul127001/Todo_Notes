a1883e331066981dd5117c7edcdbcd3f421b62db
Khan Abdul Karim <axel127001@gmail.com>
2025-02-23 21:57:06 +0530

    Added Add notes

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const Add_Card = document.querySelector('.add-card'),
 Model_box = document.querySelector('.model'),
 Close_Model = document.querySelector('header i'),
 Add_Note = document.querySelector('main button'),
+Title = document.querySelector("#title"),
+Message = document.querySelector("textarea"),
 Month_Arr = ["Jan","Feb","March","April","May","June","july","Aug","Sep","Oct","Nov","Dec"];
 const notes = JSON.parse(localStorage.getItem("notes") || "[]"); 
 
@@ -12,9 +14,10 @@ Add_Card.addEventListener('click', () => {
 })
 
 Close_Model.addEventListener('click', () => {
+    Title.value="";
+    Message.value="";
     Model_box.classList.remove('show');  
 })
-
 
 window.onclick = function(event) {
     if (event.target == Model_box) {
@@ -22,9 +25,37 @@ window.onclick = function(event) {
     }
 }
 
+let ShowNotes = () => {
+    document.querySelectorAll('.note').forEach(note=>note.remove());
+    notes.map((note)=>{
+        let New_Note = ` <li class="note">
+                <div class="details">
+                    <p>${note.title}</p>
+                    <span>${note.message}</span>
+                </div>
+                <div class="details-footer">
+                    <span>${note.date}</span>
+                    <div class="settings">
+                        <i class="fa-solid fa-ellipsis"></i>
+                        <ul class="menu">
+                            <li><i class="fa-solid fa-pen-to-square" onclick="Edit(this)"></i>Edit</li>
+                            <li><i class="fa-solid fa-trash-can"></i>Delete</li>
+                        </ul>
+                    </div>
+                </div>
+            </li>`;
+        Add_Card.insertAdjacentHTML('afterend', New_Note);
+    })
+}
+
+ShowNotes();
+
+const Edit = (elem) => {
+console.log(elem);
+}
+
+
 Add_Note.addEventListener('click', () => {
-  let Title = document.querySelector("#title"),
-  Message = document.querySelector("textarea");
 //   console.log(Title.value, Message.value)
 if(Title.value || Message.value) {
     let Cur_Date = new Date(),
@@ -42,28 +73,12 @@ if(Title.value || Message.value) {
 
    
     notes.push(Details);
-    localStorage.setItem("notes", JSON.stringify(notes))
+    localStorage.setItem("notes", JSON.stringify(notes));
+    Close_Model.click();
+    ShowNotes();
 
-
-    let New_Note = ` <li class="note">
-                <div class="details">
-                    <p>${Details.title}</p>
-                    <span>${Details.message}</span>
-                </div>
-                <div class="details-footer">
-                    <span>${Details.date}</span>
-                    <div class="settings">
-                        <i class="fa-solid fa-ellipsis"></i>
-                        <ul class="menu">
-                            <li><i class="fa-solid fa-pen-to-square"></i>Edit</li>
-                            <li><i class="fa-solid fa-trash-can"></i>Delete</li>
-                        </ul>
-                    </div>
-                </div>
-            </li>`;
-} else {
-    alert("All Feilds are required")
-}
+    
+} 
 
 })
 
